5e2eebe56f50022962d3ea5b370bdb34f6bb2889
rosemikey13 <rosemikey683@gmail.com>
2020-01-28 13:19:10 -0500

    "Added Tech forms test object"

--- app/src/main/java/com/example/meridian_v2/MainActivity.java
+++ app/src/main/java/com/example/meridian_v2/MainActivity.java
@@ -10,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* TechTrip T = new TechTrip();
+        T.setTechTripNum("3");
+        T.setRelatedBirdSurv("BSURV#1322567");
+        T.setMileage("43");
+        T.setSiteVisitComments("Site in disrepair");
+        T.setBRT("John Cena");
+        T.setTechIssues("Unable to access attic");
+        T.setIssueDevice("Asus Tab");
+        T.setIssueDetails("None to report");
+        T.setTechExperFeed("Cool");
+        T.setTechExperFeedDate("12/13/2019");
+        T.setStartTimeIntDate("3:00 p.m.");
+        T.setStartTimeIntTime("4:00 p.m.");
+        T.setTimeOutIntDate("2/16/2020");
+        T.setTimeOutIntTime("6:00 p.m.");
+        T.setStartTimeExtDate("2/12/2021");
+        T.setTimeOutExtDate("7/27/2021");
+        T.setTimeOutExtTime("4:00 p.m.");*/
+
     }
 }
